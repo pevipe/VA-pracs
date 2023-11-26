2e45428eec60740ccd2258ebaa7df13290347370
Pelayo <pelayo.vieites.perez@udc.es>
2023-11-26 20:40:08 +0100

    Movidos ejemplos y función show_results

--- p1/base.py
+++ p1/base.py
@@ -3,6 +3,7 @@ import cv2 as cv
 from time import time
 from matplotlib import pyplot as plt
 
+
 # Para testing
 def plot_2_with_hist(inImage, outImage):
     plt.figure(figsize=(12,6))
@@ -36,7 +37,28 @@ def plot_2_with_hist(inImage, outImage):
     plt.tight_layout()
     plt.show()
 
+
 def plot_comparison(img1, img2, global_title='Test', text1='Original', text2='Modificada'):
+    plt.figure(figsize=(12,6))
+    plt.title(global_title)
+
+    # Imagen original
+    plt.subplot(1,2,1)
+    plt.imshow(img1, cmap='gray')
+    plt.title(text1)
+    plt.axis('off')
+
+    # Histograma original
+    plt.subplot(1,2,2)
+    plt.imshow(img2, cmap='gray')
+    plt.title(text2)
+    plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+
+def show_results(img1, img2, global_title='Test', text1='Original', text2='Modificada'):
     plt.figure(figsize=(12,6))
     plt.title(global_title)
 
