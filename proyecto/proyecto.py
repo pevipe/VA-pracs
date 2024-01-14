05850c757d1d74aa63403c16ad23d52c1cf428bc
Pelayo <pelayo.vieites.perez@udc.es>
2024-01-14 12:11:31 +0100

    Mejorada segmentación de artefactos

--- proyecto/proyecto.py
+++ proyecto/proyecto.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import cv2 as cv
 import os
 import numpy as np
@@ -59,14 +61,56 @@ def fill_circle(image, circle, scale):
     return out_image / 255
 
 
-def get_artifacts(in_image, pupil, iris):
-    # Get general objects
-    canny = cv.Canny(in_image, 150, 200)
 
-    binary_pupil_artifacts = fill_circle(in_image, pupil, 0.85) * canny
-    binary_iris_artifacts = (fill_circle(in_image, iris, 0.85) - fill_circle(in_image, pupil, 1.25)) * canny
+def get_artifacts(in_image, pupil, iris):
+    # Máscaras binarias de iris y pupila
+    pupil_circle = fill_circle(in_image, pupil, 0.85)
+    iris_circle = fill_circle(in_image, iris, 0.95) - fill_circle(in_image, pupil, 1.05)
+
+    # Imágenes con píxeles de las zonas de pupila o iris
+    image_pupil = (in_image * pupil_circle).astype(np.uint8)
+    image_iris = (in_image * iris_circle).astype(np.uint8)
+
+    # Índices de las zonas de iris y pupila
+    indices_to_keep_pupil = np.nonzero(pupil_circle)
+    indices_to_keep_iris = np.nonzero(iris_circle)
+
+    # Artefactos pupila: threshold de 50 es suficiente
+    _, art_pupila = cv.threshold(image_pupil, 50, 255, cv.THRESH_BINARY)
+    art_pupila = art_pupila * pupil_circle
+
+    # Para el iris:
+    # 1. Ecualizo el histograma de las zonas de interés y hago la media de ese valor con el de ese píxel en la imagen
+    #    original
+    # 2. Hago el threshold sobre esos valores (adaptativa, en función de la media de la región de iris)
+    # 3. Creo una matriz resultado con la imagen a 0
+    # 4. Para los índices del iris, copio los valores del threshold
+
+    # Primero, un pequeño suavizado
+    blur = cv.blur(image_iris, (3, 3), 0)
+    # Aplicar ecualización
+    eq1 = cv.equalizeHist(blur[indices_to_keep_iris])
+    for i in range(len(eq1)):
+        # Hacer media ponderada entre la imagen ecualizada y la suavizada TODO: en función de la media?
+        s = (eq1[i][0].astype(np.uint16)*2 + blur[indices_to_keep_iris[0][i], indices_to_keep_iris[1][i]].astype(np.uint16))
+        eq1[i][0] = s // 3
+    mean_iris = np.mean(eq1)
+    white_threshold = mean_iris + (255-mean_iris) * .58
+    black_threshold = mean_iris * .3
+
+    # Aplicar thresholds superior e inferior (se consideran artefactos los que están por debajo del primero y por
+    # encima del segundo). Después, sumar los arrays (disjuntos)
+    _, art_iris1 = cv.threshold(eq1, black_threshold, 255, cv.THRESH_BINARY_INV)
+    _, art_iris2 = cv.threshold(eq1, white_threshold, 255, cv.THRESH_BINARY)
+    art_iris = art_iris1 + art_iris2
+
+    # De volver a forma matricial los datos de threshold para los índices del iris
+    art_iris_mat = np.zeros_like(in_image)
+    for i in range(len(indices_to_keep_iris[0])):
+        art_iris_mat[indices_to_keep_iris[0][i], indices_to_keep_iris[1][i]] = art_iris[i][0]
 
     return binary_pupil_artifacts, binary_iris_artifacts
+    return art_pupila, art_iris_mat
 
 
 def detect_eye(image):
@@ -105,10 +149,13 @@ def detect_eye(image):
         # Draw the artifacts in the image
         image_color[pupil_artifacts_coordinates] = [0, 0, 255]      # Pupil's artifacts in red
         image_color[iris_artifacts_coordinates] = [255, 255, 0]     # Iris' artifacts in blue
+        image_color[pupil_artifacts_coordinates] = [0, 0, 255]  # Pupil's artifacts in blue
+        image_color[iris_artifacts_coordinates] = [255, 255, 0]  # Iris' artifacts in yellow
 
     # Show image with segmentations
-    cv.imshow("Detected segmentations", image_color)
-    cv.waitKey(0)
+    # cv.imshow("Detected segmentations", image_color)
+    # cv.waitKey(0)
+    show_results(image, image_color)
 
 detect_eye()
 
